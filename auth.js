353d077228d9a3364d936bfdce92cf0ed1f062bd
BRobinson404 <brobinso123@gmail.com>
2023-05-18 18:11:14 -0500

    undoing JWT due to not working

--- auth.js
+++ auth.js
@@ -1,10 +1,8 @@
 const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
 
-const jwt = require('jsonwebtoken'),
-  passport = require('passport');
-
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 require('./passport'); // Your local passport file
-
 
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
@@ -12,13 +10,12 @@ let generateJWTToken = (user) => {
     expiresIn: '7d', // This specifies that the token will expire in 7 days
     algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
   });
-}
-
+};
 
 /* POST login. */
 module.exports = (router) => {
   router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
+    passport.authenticate('local', { session: false }, (error, user) => {
       if (error || !user) {
         return res.status(400).json({
           message: 'Something is not right',
@@ -34,4 +31,4 @@ module.exports = (router) => {
       });
     })(req, res);
   });
-}
+};
