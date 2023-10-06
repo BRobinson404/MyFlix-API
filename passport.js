fefb71044951d7170de1ef1526134762aed28cec
BRobinson404 <brobinso123@gmail.com>
2023-10-05 20:51:47 -0500

    added documentation
    created comments for JSDOC

--- passport.js
+++ passport.js
@@ -7,12 +7,24 @@ let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
+/**
+ * Defines a local authentication strategy using Passport.js.
+ *
+ * @type {passport.Strategy}
+ */
 passport.use(
   new LocalStrategy(
     {
       usernameField: "Username",
       passwordField: "Password",
     },
+    /**
+     * Callback function for local authentication strategy.
+     *
+     * @param {string} username - The username provided by the user.
+     * @param {string} password - The password provided by the user.
+     * @param {function} callback - The callback function to be called upon completion.
+     */
     async (username, password, callback) => {
       console.log(username + "  " + password);
       
@@ -42,12 +54,23 @@ passport.use(
   )
 );
 
+/**
+ * Defines a JWT authentication strategy using Passport.js.
+ *
+ * @type {passport.Strategy}
+ */
 passport.use(
   new JWTStrategy(
     {
       jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
       secretOrKey: "your_jwt_secret",
     },
+    /**
+     * Callback function for JWT authentication strategy.
+     *
+     * @param {Object} jwtPayload - The JWT payload containing user information.
+     * @param {function} callback - The callback function to be called upon completion.
+     */
     (jwtPayload, callback) => {
       return Users.findById(jwtPayload._id)
         .then((user) => {
